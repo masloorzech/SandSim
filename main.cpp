a388889177436948eb278312e62cdaa073593da1
Masloorzech <antoniiwan02@gmail.com>
2025-05-02 22:28:32 +0200

    adding map

--- main.cpp
+++ main.cpp
@@ -2,19 +2,39 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 
+#define PIXEL_SIZE 25
+
+void draw_map(sf::RenderWindow window,bool** map, size_t width, size_t height){
+
+    sf::RectangleShape tile(sf::Vector2f(PIXEL_SIZE, PIXEL_SIZE));
+    tile.setFillColor(sf::Color::Black);
+    tile.setOutlineThickness(1);
+    tile.setOutlineColor(sf::Color::White);
+
+    for (size_t i=0; i<height; i++){
+        for (size_t j =0; j<width; j++){
+            if (*(*(map + i) + j)){
+                tile.setFillColor(sf::Color::White);
+                window.draw(tile);
+                tile.setFillColor(sf::Color::Black);
+            }
+            else {
+                tile.setPosition(sf::Vector2f(i * PIXEL_SIZE, j * PIXEL_SIZE));
+                window.draw(tile);
+            }
+        }
+    }
+
+}
 
 int main(){
     sf::RenderWindow window = sf::RenderWindow(sf::VideoMode({800,600}), "SandSim");
 
     int width =20;
     int height =20;
-    int pixel_size = 20;
     bool map[width][height];
 
-    sf::RectangleShape tile(sf::Vector2f(pixel_size, pixel_size));
-    tile.setFillColor(sf::Color::Black);
-    tile.setOutlineThickness(1);
-    tile.setOutlineColor(sf::Color::White);
+
 
     while (window.isOpen()){
 
@@ -25,12 +45,6 @@ int main(){
 
         window.clear();
 
-        for (int i=0; i<height; i++){
-            for (int j =0; j<width; j++){
-                tile.setPosition(sf::Vector2f(i*pixel_size,j*pixel_size));
-                window.draw(tile);
-            }
-        }
 
         window.display();
     }

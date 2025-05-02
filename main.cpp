7771b1c3ad5f113173f8e77cb62d2a45039ae543
Masloorzech <antoniiwan02@gmail.com>
2025-05-02 22:42:38 +0200

    adding map

--- main.cpp
+++ main.cpp
@@ -4,39 +4,35 @@
 
 #define PIXEL_SIZE 25
 
-void draw_map(sf::RenderWindow window,bool** map, size_t width, size_t height){
-
+void draw_map(sf::RenderWindow &window, const std::vector<std::vector<bool>> &map, size_t width, size_t height) {
     sf::RectangleShape tile(sf::Vector2f(PIXEL_SIZE, PIXEL_SIZE));
-    tile.setFillColor(sf::Color::Black);
     tile.setOutlineThickness(1);
     tile.setOutlineColor(sf::Color::White);
 
-    for (size_t i=0; i<height; i++){
-        for (size_t j =0; j<width; j++){
-            if (*(*(map + i) + j)){
+    for (size_t y = 0; y < height; y++) {
+        for (size_t x = 0; x < width; x++) {
+            tile.setPosition(sf::Vector2f (x* PIXEL_SIZE, y * PIXEL_SIZE));
+            if (map[y][x]) {
                 tile.setFillColor(sf::Color::White);
-                window.draw(tile);
+            } else {
                 tile.setFillColor(sf::Color::Black);
             }
-            else {
-                tile.setPosition(sf::Vector2f(i * PIXEL_SIZE, j * PIXEL_SIZE));
-                window.draw(tile);
-            }
+            window.draw(tile);
         }
     }
-
 }
 
-int main(){
-    sf::RenderWindow window = sf::RenderWindow(sf::VideoMode({800,600}), "SandSim");
 
-    int width =20;
-    int height =20;
-    bool map[width][height];
+int main() {
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SandSim");
 
+    const size_t width = 20;
+    const size_t height = 20;
+    std::vector<std::vector<bool>> map(height, std::vector<bool>(width)); // [y][x]
 
+    map[0][10] = true;
 
-    while (window.isOpen()){
+    while (window.isOpen()) {
 
         while (const std::optional event = window.pollEvent()){
             if (event->is<sf::Event::Closed>())
@@ -45,10 +41,12 @@ int main(){
 
         window.clear();
 
+        draw_map(window, map, width, height);
+
+
 
         window.display();
     }
 
     return 0;
-
 }

2bf969304112137f7f5523503d6b8f372c8d1467
Masloorzech <antoniiwan02@gmail.com>
2025-05-02 22:23:21 +0200

    adding map

--- main.cpp
+++ main.cpp
@@ -6,14 +6,32 @@
 int main(){
     sf::RenderWindow window = sf::RenderWindow(sf::VideoMode({800,600}), "SandSim");
 
+    int width =20;
+    int height =20;
+    int pixel_size = 20;
+    bool map[width][height];
+
+    sf::RectangleShape tile(sf::Vector2f(pixel_size, pixel_size));
+    tile.setFillColor(sf::Color::Black);
+    tile.setOutlineThickness(1);
+    tile.setOutlineColor(sf::Color::White);
+
     while (window.isOpen()){
 
-        while (const std::optional event = window.pollEvent())
-        {
+        while (const std::optional event = window.pollEvent()){
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
+
         window.clear();
+
+        for (int i=0; i<height; i++){
+            for (int j =0; j<width; j++){
+                tile.setPosition(sf::Vector2f(i*pixel_size,j*pixel_size));
+                window.draw(tile);
+            }
+        }
+
         window.display();
     }
 
